24249d2aad59ca7208c5e287fafaa492a0e5651e
Chipy <iamchipy@gmail.com>
2023-02-11 18:58:04 -0500

    v1

--- chipys-5e-tools/chipys_5e_tools/dice.py
+++ chipys-5e-tools/chipys_5e_tools/dice.py
@@ -9,15 +9,15 @@ class Dice:
     def __init__(self):
         pass
 
-    def r(self, in_str:str="1d20", advantage:bool=False, show_rolls:bool= False, flag_adv:bool= False, flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_pam:bool= False):
+    def r(self, in_str:str="1d20", flag_adv:bool=False, show_rolls:bool= False, flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_gwm:bool= False, flag_spec:int=0):
         """"alias for self.formula()"""
-        return self.formula(in_str, advantage, show_rolls, flag_adv, flag_bls, flag_dis, flag_ela, flag_ins, flag_pam)
+        return self.formula(in_str, flag_adv, show_rolls, flag_bls, flag_dis, flag_ela, flag_ins, flag_gwm, flag_spec)
 
-    def f(self, in_str:str="1d20", advantage:bool=False, show_rolls:bool= False, flag_adv:bool= False, flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_pam:bool= False):
+    def f(self, in_str:str="1d20", flag_adv:bool=False, show_rolls:bool= False,flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_gwm:bool= False, flag_spec:int=0):
         """"alias for self.formula()"""
-        return self.formula(in_str, advantage, show_rolls, flag_adv, flag_bls, flag_dis, flag_ela, flag_ins, flag_pam)
+        return self.formula(in_str, flag_adv, show_rolls, flag_bls, flag_dis, flag_ela, flag_ins, flag_gwm, flag_spec)
 
-    def formula(self, in_str:str="1d20", advantage:bool=False, show_rolls:bool= False, flag_adv:bool= False, flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_pam:bool= False):
+    def formula(self, in_str:str="1d20", flag_adv:bool=False, show_rolls:bool= False, flag_bls:bool= False,flag_dis:bool= False,flag_ela:bool= False,flag_ins:bool= False,flag_gwm:bool= False, flag_spec:int=0):
         """_summary_
 
         Args:
@@ -32,10 +32,20 @@ class Dice:
         adv_rolled = False
         int_value = 0
         formula = in_str.split("+")  # | (pipe) separates delimiters
+        # Blessed gets handled here ------------- flag_bls
+        if flag_bls:
+            formula.append("1d4")
+            
         for element in formula:
             if "d" in element:
-                if advantage and not adv_rolled:
-                    i, s = self.roll(element, advantage=True)
+                # Advantage get handled here ------------- flag_adv
+                # Disadvantage get handled here ------------- flag_dis
+                # ElvenAccuracy get handled here ------------- flag_ela
+                # Inspiration get handled here ------------- flag_ins
+
+                # all of these are only being applied to the first dice in the formula and only once 
+                if not adv_rolled:
+                    i, s = self.roll(element, flag_adv=flag_adv, flag_dis=flag_dis, flag_ins=flag_ins, flag_ela=flag_ela)
                     dice_report += str(s)
                     int_value += i
                     adv_rolled = True
@@ -45,36 +55,72 @@ class Dice:
                     int_value += i                    
             else:
                 int_value += int(element)
+
+        # SharpShooter/GreatWepMaster gets handled here ------------- flag_gwm
+        if flag_gwm:
+            int_value -= 5
+
+        # SPECIAL gets handled here ------------- flag_gwm
+        int_value += flag_spec            
+        
+        # check what info to return and return it
         if show_rolls:
             return int_value, dice_report, in_str
         else:
             return int_value
     
-    def roll(self, dice_string="1d20", advantage=False ):
-        """Rolls Dice of #d## format string
+    def roll(self, dice_string="1d20", flag_adv:bool=False, flag_dis:bool=False, flag_ins:bool=False, flag_ela:bool=False)-> int:
+        """_summary_
 
         Args:
-            dice_string (String): A dice roll string in the #d## format
-            advantage (Bool): A toggle indicating if there should be advantage (it'll roll one extra dice and drop lowest)
+            dice_string (str, optional): Dice formula. Defaults to "1d20".
+            flag_adv (bool, optional): A toggle indicating if there should be advantage (it'll roll one extra dice and drop lowest). Defaults to False.
+            flag_dis (bool, optional): A toggle indicating if there should be disadvantage (it'll roll one extra dice and drop highest). Defaults to False.
+            flag_ins (bool, optional): A toggle indicating if there should be advantage (it'll roll one extra dice and drop lowest). Defaults to False.
+            flag_ela (bool, optional): A toggle to roll a unique stacking advantage. Defaults to False.
 
         Returns:
-            int: value the dice rolled 
+            int: dice roll
         """
-        # int_value=0
+        # calc advantange
+        adv_counter = 0
+        if flag_adv or flag_ins:
+            adv_counter +=1
+        if flag_dis:
+            adv_counter -=1          
+
         adv_rolled = False
         dice_rolls = []
+        kept_rolls = []
         dice_array = dice_string.split("d")
+        # loop ones for each dice being roll 
         for i in range(int(dice_array[0])):
-            if advantage and not adv_rolled:
-                dice_rolls.append(self._roll_value(dice_array[1]))
-                adv_rolled = True
-            dice_rolls.append(self._roll_value(dice_array[1]))        
-        if advantage:
-            dice_rolls.sort()
-            lowest_roll_dropped = dice_rolls[1:]
-            return sum(lowest_roll_dropped), dice_rolls
-        else:
-            return sum(dice_rolls), dice_rolls
+            # base dice
+            dice_rolls.append(self._roll_value(dice_array[1]))
+            kept_rolls = dice_rolls
+
+            # if we've not yet rolled advantages for this dice formula yet (can't only get one adv)
+            # we do this on the first roll so that we can manipulate the list safely
+            if not adv_rolled:
+                # if there are adv/dis to be applied roll a second dice
+                if adv_counter!=0:
+                    dice_rolls.append(self._roll_value(dice_array[1]))
+                    adv_rolled = True
+                    # if we have positive advantage then drop lower of the two
+                    if adv_counter>0:
+                        # if we have Elven Accuracy we do a 3rd dice and drop lowest
+                        if flag_ela:
+                            dice_rolls.append(self._roll_value(dice_array[1]))
+                            dice_rolls.sort()
+                            kept_rolls = dice_rolls[2:]
+                        else:
+                            dice_rolls.sort()
+                            kept_rolls = dice_rolls[1:]
+                    else:
+                        dice_rolls.sort()
+                        kept_rolls = dice_rolls[:1]                     
+
+        return sum(kept_rolls), dice_rolls
 
     def _roll_value(self, sides_to_roll=0):
         """Most basic roll command to generate a random number within the range
@@ -105,4 +151,6 @@ if __name__ == "__main__":
     print("2d20 ", d.r("2d20"))
     print("2d20 a", d.r("2d20",True))
     print("2d20 a", d.r("2d20",True,True))
-    print("2d20 a", *d.r("2d20+2",True,True))
+    print("2d20 a", d.r("4d20",show_rolls=True, flag_adv=1, flag_ela=1))
+    print("1d20 ", d.r("1d20",show_rolls=1,flag_gwm=1))
+    print("1d20 ", d.r("1d20",show_rolls=1,flag_spec=100))
--- chipys-5e-tools/chipys_5e_tools/main.py
+++ chipys-5e-tools/chipys_5e_tools/main.py
@@ -11,12 +11,20 @@ dice_log_model = None
 ui = None
 dice_tower = dice.Dice()
 
-def apply_ui_connections(gui_obj:QT.gui.Ui_MainWindow):
+def apply_ui_connections(gui_obj:QT.gui.Ui_MainWindow, win_hndl:QtWidgets.QMainWindow):
     """Overlay that connects up the GUI so that we can modularly replace the gui.py from QT5
 
     Args:
         gui_obj (gui.Ui_MainWindow): Main window GUI object
     """
+    # set window icon
+    win_hndl.setWindowIcon(QtGui.QIcon('Chipy128.ico'))
+    win_hndl.setWindowTitle("Chipy's 5E Dice Sim")
+
+    # link menus
+    gui_obj.actionProject_GitHub.triggered.connect(lambda: show_popup("Thanks for your curiosity! Please feel free to check out the project at https://github.com/iamchipy/chipys-5e-companion"))
+    gui_obj.actionChipy_Dev.triggered.connect(lambda: show_popup("Thanks for your curiosity! You can find more of my stuff at www.chipy.dev"))
+
     # link buttons
     gui_obj.simulate.clicked.connect(lambda: run_sim(gui_obj))
     gui_obj.roll_active.clicked.connect(lambda: roll_active(gui_obj))
@@ -25,9 +33,11 @@ def apply_ui_connections(gui_obj:QT.gui.Ui_MainWindow):
     gui_obj.dice_log.clicked[QtCore.QModelIndex].connect(click_dice_log)
     gui_obj.formula_log.clicked[QtCore.QModelIndex].connect(click_formula_log)
 
-def show_popup(gui_obj):
+def show_popup( in_str):
     msg = QtWidgets.QMessageBox()
     msg.setWindowTitle("Results!")
+    msg.setText(in_str)
+    # msg.setIcon()
     msg_instance = msg.exec_()  # exec with instanciation
 
 def click_dice_log(index):
@@ -53,7 +63,9 @@ def roll_active(gui_obj):
     flag_dis = gui_obj.flag_dis.isChecked()
     flag_ela = gui_obj.flag_ela.isChecked()
     flag_ins = gui_obj.flag_ins.isChecked()
-    flag_pam = gui_obj.flag_pam.isChecked()
+    flag_gwm = gui_obj.flag_gwm.isChecked()
+    ac= gui_obj.armor_class.value()
+    hit_rate = [0,0]
   
 
     roll, log, formula = dice_tower.r(formula, show_rolls=True,
@@ -62,15 +74,19 @@ def roll_active(gui_obj):
                                                 flag_dis = flag_dis,
                                                 flag_ela = flag_ela,
                                                 flag_ins = flag_ins,
-                                                flag_pam = flag_pam)
+                                                flag_gwm = flag_gwm)
 
     # build log
-    dice_log.append([formula,log])
+    dice_log.append([formula,roll,log])
     dice_log_model = QtGui.QStandardItemModel()
     gui_obj.dice_log.setModel(dice_log_model)
     dice_log_model.clear()
     for r in dice_log[-8:]:
         dice_log_model.appendRow(QtGui.QStandardItem(str(r)))
+        # log hits
+        if  int(r[1]) > ac:
+            hit_rate[0] +=1
+        hit_rate[1] +=1
 
     # build log last used formula
     expanded_var = formula_log[-1:][0]
@@ -82,8 +98,12 @@ def roll_active(gui_obj):
     for r in formula_log[-8:]:
         formula_log_model.appendRow(QtGui.QStandardItem(str(r)))        
 
+    # build hit rate
+    hit_rate = (100*hit_rate[0])//hit_rate[1]
+
     # display the current roll results
     gui_obj.result.setText(str(roll))
+    gui_obj.hit_chance.setText(str(hit_rate))
     return roll
     
 def run_sim(gui_obj):
@@ -91,16 +111,34 @@ def run_sim(gui_obj):
     gui_obj.result.setText("Init...")
     max_roll = dice_tower.max_roll(gui_obj.attack_formula.text())
     roll_tally = [0 for i in range(max_roll)]
+    hit_tally = [0 for i in range(35)]
     sim_count = gui_obj.sim_count.text()
-    print(sim_count)
+    ac= gui_obj.armor_class.value()
+    print(f"Starting {sim_count} itterations...")
 
     for i in range(int(sim_count)):
+    # for i in range(1000):
+        # roll dice
         rolled = roll_active(gui_obj)
-        # print(roll_tally)
+        
+        # log the roll in our tallies
         roll_tally[rolled-1] += 1
-    
+        if ac <0:
+            for dc in range(len(hit_tally)):
+                if rolled >= dc:
+                    hit_tally[dc-1] +=1
+        elif rolled >= ac:
+            hit_tally[ac-1] += 1
+
+    roll_tally = roll_tally[1:]
+    print("How hard did you hit:")
     print(roll_tally)
-    print([round((roll_tally[i]/int(sim_count))*100,1) for i in range(max_roll)])
+    print([round((roll_tally[i]/int(sim_count))*100,1) for i in range(max_roll-1)])
+
+    hit_tally = hit_tally[:-1]
+    print("How often did you hit(at what AC):")
+    print(hit_tally)
+    print([round((hit_tally[i]/int(sim_count))*100,1) for i in range(len(hit_tally))])    
     gui_obj.result.setText("Done!")
         
 
@@ -110,6 +148,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = QT.gui.Ui_MainWindow()
     ui.setupUi(MainWindow)
-    apply_ui_connections(ui)  # here we modify actions to the GUI
+    apply_ui_connections(ui, MainWindow)  # here we modify actions to the GUI
     MainWindow.show()
     sys.exit(app.exec_())
