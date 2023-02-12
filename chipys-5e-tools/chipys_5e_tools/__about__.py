5d499fc38c6511d2b79af523a042c398f4309a63
Chipy <iamchipy@gmail.com>
2023-02-12 13:09:06 -0500

    improved spacing on UI and set default AC to -1

--- chipys-5e-tools/chipys_5e_tools/__about__.py
+++ chipys-5e-tools/chipys_5e_tools/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Chipy <iamchipy@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = '0.1.1'
+__version__ = '0.1.3'
 
 print("Loading chipys_5e_tools v"+__version__)
 
